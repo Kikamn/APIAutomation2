4975efa80baa44792d710b1f12636cf127572988
DaffaPintu <daffa@pintu.co.id>
2024-04-25 08:09:03 +0700

    FIX

--- src/test/java/helper/Endpoint.java
+++ src/test/java/helper/Endpoint.java
@@ -3,7 +3,7 @@ package helper;
 import java.security.PublicKey;
 
 public class Endpoint {
-    public static final String host_gorest = "https://gorest.co.in/public/v2";
+    public static final String host_gorest = "https://gorest.co.in/public/v2/";
     public static final String GET_LIST_USERS = host_gorest + "users";
     public static final String CREATE_NEW_USER = host_gorest + "users2";
     public static final String DELETE_USERS = host_gorest + "users3";
